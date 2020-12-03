980dce1d7e58725e1109b5f2a2927dd5d1e65106
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2020-12-03 17:23:00 +0000

    Add files via upload

---
+++ PropertyList.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class PropertyList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PropertyList
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class PropertyList
+     */
+    public PropertyList()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
