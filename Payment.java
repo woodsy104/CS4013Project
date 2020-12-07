312523dbdcf8573f3a40d7b55444717e11c00ba9
AndrewMcMahon123 <53841605+AndrewMcMahon123@users.noreply.github.com>
2020-12-07 14:10:04 +0000

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -7,6 +7,7 @@ public class Payment{
     double propTax;
     String status;
     double propValue;
+    ArrayList<Property> propertyList;
     
     public Payment(String owner, String eircode, String address, double propValue, double toPay){
      this.propValue = propValue;
@@ -18,16 +19,32 @@ public class Payment{
      this.toPay = this.propTax;   
     }
     
+    public void propertyList(ArrayList<Property> propertyList){
+        this.propertyList = propertyList;
+        
+    }
+    
     public void payment(double amount){
         this.toPay = amount - this.propTax;
+        int tPay = 0;
        if(this.toPay == 0){
             this.status = "P";
         }
         else{
         this.status = "NP";
-        
-        
-        
+            paymentRecord list = new paymentRecord();
+            String[] pay = list.readToArray("payment.csv");
+            String sPay = "";
+        for(int i=0; i<pay.length; i++){
+            if(pay[i].equals(this.owner)){
+             sPay = pay[i+4];
+             tPay = Integer.parseInt(sPay);
+            }
+        }
+        this.toPay -= tPay;
+        if(this.toPay == 0){
+            this.status = "P";
+        }
     }
     }
     
