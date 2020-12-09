72edcc1efcfb01c2e685ba3dee1737a2cf0b507d
woodsy104 <55923959+woodsy104@users.noreply.github.com>
2020-12-09 16:18:30 +0000

    Update PropertyTax.java

--- PropertyTax.java
+++ PropertyTax.java
@@ -20,7 +20,7 @@ public class PropertyTax {
     private static int getLocationRate(String location){
         int locationRate = 0;
         for (int i = 0; i < locations.length - 1; i++){
-            if(location.equals(locations[i])){
+            if(location.equalsIgnoreCase(locations[i])){
                 locationRate = locationBase[i];
             }
         }
